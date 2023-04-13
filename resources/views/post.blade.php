665b8dfd058a3807ba19b581d9e3d562be0b1982
Arodi Sedassie <arsedassie@speednet-wireless.com>
2023-04-13 14:33:14 -0600

    for blog

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+    <article>
+
+        <?= $post; ?> 
+
+    </article>
+
+    <a href="/">Go Back</a>
+
+    
+</body>
